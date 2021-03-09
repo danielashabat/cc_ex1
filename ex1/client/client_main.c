6d9781e56372870c0c4168dfcf01d175bb58364d
אור אוחיון <meiroroha@gmail.com>
2021-03-09 19:03:25 +0200

    setup envrioment

---
+++ ex1/client/client_main.c
@@ -0,0 +1,14 @@
+
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+/*Authors -Daniela Shabat 316415645,<add here >.
+Project – 
+Description –  
+ */
+ /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
+
+
+int main(int argc, char* argv[]) {
+	printf("hello client\n");
+	return 0;
+}
