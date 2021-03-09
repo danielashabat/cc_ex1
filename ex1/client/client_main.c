299634c8ccbf6a351adea0eb39daf19c5467e5ae
אור אוחיון <meiroroha@gmail.com>
2021-03-09 19:49:12 +0200

    need to remove errors

--- ex1/client/client_main.c
+++ ex1/client/client_main.c
@@ -1,14 +1,77 @@
 
 /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 /*Authors -Daniela Shabat 316415645,<add here >.
-Project – 
+Project – client
 Description –  
  */
  /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
 
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <winsock2.h>
+
+#define SERVER_ADDRESS_STR "127.0.0.1"
+
+
+//debug 
+#define DEBUG(MSG) printf("function:[%s line:%d] %s\n", __func__, __LINE__,MSG)
+
+typedef enum { FAIL=1} return_values;
+
+//
 
 int main(int argc, char* argv[]) {
-	printf("hello client\n");
+
+	char IP[] = SERVER_ADDRESS_STR;
+	int port = 8888;
+
+	SOCKET client_socket;
+	SOCKADDR_IN clientService;
+
+	// Initialize Winsock.
+	WSADATA wsaData; //Create a WSADATA object called wsaData.
+					 //The WSADATA structure contains information about the Windows Sockets implementation.
+
+					 //Call WSAStartup and check for errors.
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR) {
+		DEBUG("Error at WSAStartup()\n");
+		return FAIL;
+	}
+	// Create a socket.
+	client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_TCP);
+
+	// Check for errors to ensure that the socket is a valid socket.
+	if (client_socket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+
+	//can remove connect in UDP
+
+	//Create a sockaddr_in object clientService and set  values.
+	clientService.sin_family = AF_INET;
+	clientService.sin_addr.s_addr = inet_addr(IP); //Setting the IP address to connect to
+	clientService.sin_port = htons(port); //Setting the port to connect to.
+
+										  /*
+										  AF_INET is the Internet address family.
+										  */
+										  // Call the connect function, passing the created socket and the sockaddr_in structure as parameters. 
+										  // Check for general errors.
+
+	if (SOCKET_ERROR == connect(client_socket, (SOCKADDR*)&clientService, sizeof(clientService))) {
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Connected to server on %s:%d\n", IP, port);
+
+	printf("bye client\n");
 	return 0;
 }
--- ex1/server/server_main.c
+++ ex1/server/server_main.c
@@ -1,14 +1,74 @@
+
+
 /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 /*Authors -Daniela Shabat 316415645,<add here >.
-Project –
+Project – server
 Description –
  */
  /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
 #include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <winsock2.h>
+
+
+#define SERVER_ADDRESS_STR "127.0.0.1"
 
 
 int main(int argc, char* argv[]) {
-	printf("hello server\n");
+    int port = 8888;
+   
+    SOCKET MainSocket = INVALID_SOCKET;
+    unsigned long Address;
+    SOCKADDR_IN service;
+    int bindRes;
+    int ListenRes;
+
+    // Initialize Winsock.
+    WSADATA wsaData;
+    int StartupRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    SOCKET AcceptSocket;
+    int program_running = 1;
+    int i = 0;
+    if (StartupRes != NO_ERROR)
+    {
+        printf("error %ld at WSAStartup( ), ending program.\n", WSAGetLastError());
+        // Tell the user that we could not find a usable WinSock DLL.                                  
+        return;
+    }
+
+    MainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (MainSocket == INVALID_SOCKET)
+    {
+        printf("Error at socket( ): %ld\n", WSAGetLastError());
+        return 1;
+    }
+
+    //create a sockaddr_in 
+
+    Address = inet_addr(SERVER_ADDRESS_STR);
+    if (Address == INADDR_NONE)
+    {
+        printf("The string \"%s\" cannot be converted into an ip address. ending program.\n",
+            SERVER_ADDRESS_STR);
+        return 1;;
+    }
+
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = Address;
+    service.sin_port = htons(port);
+    /*bind*/
+
+    bindRes = bind(MainSocket, (SOCKADDR*)&service, sizeof(service));
+    if (bindRes == SOCKET_ERROR)
+    {
+        printf("bind( ) failed with error %ld. Ending program\n", WSAGetLastError());
+        return 1;
+    }
+
+
+	printf("bye server\n");
 	return 0;
 }
